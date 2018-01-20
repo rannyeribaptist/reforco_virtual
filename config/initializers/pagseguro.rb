a926defb92ca789b82718eba548e679d1e0a19ba
Rannyeri Rodrigues Batista <rannyerirbatista@gmail.com>
2018-01-20 13:18:17 -0300

    pagseguro token on production

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,5 +1,5 @@
 PagSeguro.configure do |config|
-  config.token       = "DF824F16E3AE412B9EDE968DA2E31CB6"
+  config.token       = Rails.env.production? ? "A561B8134D6D4A1FB72BC158243A9339" : "DF824F16E3AE412B9EDE968DA2E31CB6"
   config.email       = "rannyeri@protonmail.com"
   config.environment = Rails.env.production? ? :production : :sandbox
   config.encoding    = "UTF-8"
