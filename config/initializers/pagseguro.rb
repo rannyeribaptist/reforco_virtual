34a1aa9120da1dec3ce04074e4a374da01debc88
Rannyeri Rodrigues Batista <rannyerirbatista@gmail.com>
2018-01-20 14:44:47 -0300

    changed pagseguro account and created meta tag in visitors layout to google gsuit verification

--- config/initializers/pagseguro.rb
+++ config/initializers/pagseguro.rb
@@ -1,6 +1,6 @@
 PagSeguro.configure do |config|
-  config.token       = Rails.env.production? ? "A561B8134D6D4A1FB72BC158243A9339" : "DF824F16E3AE412B9EDE968DA2E31CB6"
-  config.email       = "rannyeri@protonmail.com"
+  config.token       = Rails.env.production? ? "03A8AE640DC04F37801559F9168E6E88" : "4ED900681E4C454CBB53A4E1695C2BB9"
+  config.email       = "escolaprogressao@hotmail.com"
   config.environment = Rails.env.production? ? :production : :sandbox
   config.encoding    = "UTF-8"
 end
